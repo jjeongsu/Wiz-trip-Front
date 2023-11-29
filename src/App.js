b820ee5592b8ff39f4268c7c89460f5851c10603
jjeongsu <jeongsu.dev@gmail.com>
2023-11-30 06:32:12 +0900

    [feat] GlobalStyle & color 추가

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Header from './components/Header';
 import Main from './pages/Main';
 function App() {
@@ -7,7 +7,7 @@ function App() {
       <BrowserRouter>
         <Header />
         <Routes>
-          <Route path="/" element={<Main />}></Route>
+          <Route path="/" element={<Main />} />
         </Routes>
       </BrowserRouter>
     </>
