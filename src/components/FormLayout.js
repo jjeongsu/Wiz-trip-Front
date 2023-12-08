cb7d3bbda9a4134781e93ac144721b8b9dc72a95
jjeongsu <jeongsu.dev@gmail.com>
2023-12-09 01:23:43 +0900

    [feat] Form Layout 홈버튼 추가 #10

--- src/components/FormLayout.js
+++ src/components/FormLayout.js
@@ -1,5 +1,6 @@
 import styled from 'styled-components';
-
+import { Link } from 'react-router-dom';
+import HomeIcon from '../assets/home-icon';
 function FormLayout({ children, title }) {
   return (
     <FormLayoutBox>
@@ -10,6 +11,12 @@ function FormLayout({ children, title }) {
           공유해보세요
         </p>
         <div>{children}</div>
+
+        <HomeButton>
+          <Link to="/">
+            <HomeIcon />
+          </Link>
+        </HomeButton>
       </FormBox>
     </FormLayoutBox>
   );
@@ -31,7 +38,7 @@ const FormBox = styled.div`
   flex-direction: column;
   justify-content: center;
   align-items: center;
-
+  position: relative;
   background: #ffffff;
   box-shadow:
     -2.17893px -2.17893px 6.5368px #ffffff,
@@ -54,4 +61,18 @@ const FormBox = styled.div`
     line-height: 1.6;
     margin: 0;
   }
+`;
+const HomeButton = styled.button`
+  position: absolute;
+  width: 64px;
+  height: 64px;
+  left: -7rem;
+  top: 0;
+  background: #edf3fb;
+  box-shadow:
+    -10px -10px 30px #ffffff,
+    10px 10px 30px rgba(174, 174, 192, 0.4);
+  border-radius: 16px;
+  border: none;
+  cursor: pointer;
 `;
