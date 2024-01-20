b3b196441eebe634be99bc6935f855930a131c01
jjeongsu <jeongsu.dev@gmail.com>
2024-01-20 17:38:21 +0900

    fix: planheader conflict 해결

--- src/components/Plan/Planheader.js
+++ src/components/Plan/Planheader.js
@@ -4,24 +4,16 @@ import UpdatePlanInfo from './UpdatePlanInfo';
 import ConnectedUsers from './ConnectedUsers';
 import { useDispatch, useSelector } from 'react-redux';
 import LinkIcon from '../../assets/link-icon';
-<<<<<<< HEAD
 import { createUrl, finishTrip } from '../../apis/api/trip';
 import { useParams } from 'react-router-dom';
 import { useNavigate } from 'react-router-dom';
-const domain = 'http://localhost:3000'
-function Planheader({userIdList}) {
+const domain = 'http://localhost:3000';
+function Planheader({ userIdList }) {
   const planinfo = useSelector((state) => state.Schedule);
   const [updateform, setUpdateForm] = useState(false);
   const tripId = useParams().tripId;
   const navigate = useNavigate();
 
-=======
-import { useNavigate } from 'react-router-dom';
-function Planheader({ userIdList, tripId }) {
-  const planinfo = useSelector((state) => state.Schedule);
-  const [updateform, setUpdateForm] = useState(false);
-  const navigate = useNavigate();
->>>>>>> review
   const formatDate = (date) => {
     // 월과 일만 표시하도록 설정
     const options = { month: 'long', day: 'numeric' };
@@ -31,37 +23,26 @@ function Planheader({ userIdList, tripId }) {
   const startDate = formatDate(new Date(planinfo.startDate));
   const endDate = formatDate(new Date(planinfo.endDate));
 
-<<<<<<< HEAD
-
-  //초대 url 생성 및 클립보드 복사 
-  const handleUrl = async() =>{
+  //초대 url 생성 및 클립보드 복사
+  const handleUrl = async () => {
     const res = await createUrl(tripId);
-    if(res){
-       // 클립보드에 URL 복사
-       await navigator.clipboard.writeText(`${domain}/trips/share/${res.url}`); // res.data.url은 API 응답에서 실제 URL을 가리킵니다.
-       // 사용자에게 알림
-       alert('초대링크가 클립보드에 복사되었습니다.');
+    if (res) {
+      // 클립보드에 URL 복사
+      await navigator.clipboard.writeText(`${domain}/trips/share/${res.url}`); // res.data.url은 API 응답에서 실제 URL을 가리킵니다.
+      // 사용자에게 알림
+      alert('초대링크가 클립보드에 복사되었습니다.');
     }
+  };
 
-  }
-
-  
-  //트립 종료 
-  const handleFinishTrip = async()=>{
+  //트립 종료
+  const handleFinishTrip = async () => {
     const res = await finishTrip(tripId);
-    if(res.status==200){
+    if (res.status == 200) {
       alert('trip이 종료되었습니다.');
       //메인페이지 이동
-      navigate('/')
+      navigate('/');
     }
-  }
-=======
-  const handleDoneClick = () => {
-    // 여행을 완료 확인 모달
-
-    navigate(`/write/${tripId}`);
   };
->>>>>>> review
   return (
     <P.HeaderWrapper>
       <div className="left-container">
@@ -88,21 +69,11 @@ function Planheader({ userIdList, tripId }) {
         <ConnectedUsers userIdList={userIdList} />
       </div>
       <div className="button-container">
-<<<<<<< HEAD
         <P.InviteBtn onClick={handleUrl}>
-          <LinkIcon/>
-          <span className='text'>초대링크 복사</span> 
-        </P.InviteBtn>
-        <P.CompleteBtn onClick={handleFinishTrip}>
-          여행 완료
-        </P.CompleteBtn>
-=======
-        <P.InviteBtn>
           <LinkIcon />
           <span className="text">초대링크 복사</span>
         </P.InviteBtn>
-        <P.CompleteBtn onClick={handleDoneClick}>여행 완료</P.CompleteBtn>
->>>>>>> review
+        <P.CompleteBtn onClick={handleFinishTrip}>여행 완료</P.CompleteBtn>
       </div>
     </P.HeaderWrapper>
   );
